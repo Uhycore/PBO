abc49221043a3640d795f0580b6413944a6f9415
Uhycore <arilmubin0@gamil.com>
2024-10-11 16:06:49 +0700

    komen baris input role

--- response_input.php
+++ response_input.php
@@ -1,14 +1,14 @@
 <?php
-echo "ini adalah file testing response input";
-echo "<br>";
-echo "modul = " . $_GET['modul'] . "<br>";
-echo "fitur = " . $_GET['fitur'] . "<br>";
-echo "request method : " . $_SERVER['REQUEST_METHOD'] . "<br>";
-print_r($_POST);
-echo "<br>";
-echo "nama role :  " . $_POST['role_name'] . "<br>";
-echo "keterangan role :  " . $_POST['role_description'] . "<br>";
-echo "status role :  " . $_POST['role_status'] . "<br>";
+// echo "ini adalah file testing response input";
+// echo "<br>";
+// echo "modul = " . $_GET['modul'] . "<br>";
+// echo "fitur = " . $_GET['fitur'] . "<br>";
+// echo "request method : " . $_SERVER['REQUEST_METHOD'] . "<br>";
+// print_r($_POST);
+// echo "<br>";
+// echo "nama role :  " . $_POST['role_name'] . "<br>";
+// echo "keterangan role :  " . $_POST['role_description'] . "<br>"; 
+// echo "status role :  " . $_POST['role_status'] . "<br>";
 
 require_once 'domain_object/node_role.php';
 $obj_role = [];
